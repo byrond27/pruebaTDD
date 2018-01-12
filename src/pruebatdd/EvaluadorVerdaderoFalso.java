4f8bd21b6b55f4a2bf8326143acc1f2010042bca
Sercop-Admin <Sercop-Admin@bmdominguez>
2018-01-12 10:16:27 -0500

    Creación proyecto

---
+++ src/pruebatdd/EvaluadorVerdaderoFalso.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebatdd;
+
+/**
+ *
+ * @author Sercop-Admin
+ */
+public class EvaluadorVerdaderoFalso {
+    public boolean evalua(String s){
+        return true;
+    }
+            
+    
+}
